1406f0cc2702ec57c24198d7d928b9c71c6fe409
7DeepBlues <souyadeepb007@gmail.com>
2020-06-10 23:10:48 +0530

    get customer ready

--- routes/customers.js
+++ routes/customers.js
@@ -3,6 +3,11 @@ const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 
+router.get('/', async(req,res) => {
+    const customers = await Customer.find().sort('name')
+    res.send(customers)
+})
+
 router.post('/', async(req, res) => {
     const { error } = validate(req.body)
     if(error) return res.status(400).send(error.details[0].message)
