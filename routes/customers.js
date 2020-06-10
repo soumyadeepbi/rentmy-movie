484ddded4522d9c3de84c09e604cf622ff0bcdd7
7DeepBlues <souyadeepb007@gmail.com>
2020-06-10 22:37:44 +0530

    Customers router

---
+++ routes/customers.js
@@ -0,0 +1,22 @@
+const {Customer, validate} = require('../models/customer')
+const mongoose = require('mongoose')
+const express = require('express')
+const router = express.Router()
+
+router.post('/', async(req, res) => {
+    const { error } = validate(req.body)
+    if(error) return res.status(400).send(error.details[0].message)
+
+    let customer = new Customer({
+        name: req.body.name,
+        isGold: req.body.isGold,
+        phone: req.body.phone
+    })
+
+    customer = await customer.save()
+
+    res.send(customer)
+})
+
+
+module.exports = router
