ca9eb3c4278f465f957dc8bac11a071eaa76b71b
7DeepBlues <souyadeepb007@gmail.com>
2020-06-10 22:47:14 +0530

    debugging

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
+const mongoose = require('mongoose')
 const express = require('express')
 const customers = require('./routes/customers')
 const app = express()
 
-mongoose.connect('mongodb://localhost/rentmymovie')
+mongoose.connect('mongodb://localhost/rentmymovie',{ useNewUrlParser:true, useUnifiedTopology:true })
     .then(() => console.log('Connected to MongoDB...'))
     .catch(err => console.error('Could not connect to MongoDB...'))
 
