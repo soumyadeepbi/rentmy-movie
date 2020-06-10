28ebce2c7adbfe6915df0aba8ee98b6bd7678a2a
7DeepBlues <souyadeepb007@gmail.com>
2020-06-10 22:29:02 +0530

    Customer model

---
+++ models/customer.js
@@ -0,0 +1,33 @@
+const Joi = require('@hapi/joi')
+const mongoose = require('mongoose')
+
+const Customer = mongoose.model('Customer', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    isGold: {
+        type: Boolean,
+        default: false
+    },
+    phone: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+}))
+
+function validateCustomer(customer){
+    const schema = Joi.object({
+        name: Joi.string().min(4).max(50).required(),
+        isGold: Joi.Boolean(),
+        phone: Joi.number().min(5).max(50).required()
+    })
+    return schema.validate(customer)
+}
+
+exports.Customer = Customer
+exports.validate = validateCustomer
