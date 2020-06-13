b1d470bb90f79ca10bca59910c5ee0e144ef7599
7DeepBlues <souyadeepb007@gmail.com>
2020-06-13 23:04:26 +0530

    promise rejection handled

--- routes/customers.js
+++ routes/customers.js
@@ -1,54 +1,72 @@
-const {Customer, validate} = require('../models/customer')
+const { Customer, validate } = require('../models/customer')
 const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async(req,res) => {
-    const customers = await Customer.find().sort('name')
-    res.send(customers)
+router.get('/', async (req, res) => {
+    try {
+        const customers = await Customer.find().sort('name')
+        res.send(customers)
+    }
+    catch (err) {
+        res.status(404).send('Some error occured')
+    }
 })
 
-router.post('/', async(req, res) => {
+router.post('/', async (req, res) => {
     const { error } = validate(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    if (error) return res.status(400).send(error.details[0].message)
 
-    let customer = new Customer({
-        name: req.body.name,
-        isGold: req.body.isGold,
-        phone: req.body.phone
-    })
-
-    customer = await customer.save()
-
-    res.send(customer)
-})
-
-router.put('/:id', async(req,res) => {
-    const { error } = validate(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
-
-    const customer = await Customer.findByIdAndUpdate(req.params.id,
-        {
+    try {
+        let customer = new Customer({
             name: req.body.name,
             isGold: req.body.isGold,
             phone: req.body.phone
-        }, { new: true})
-    
-    if(!customer) return res.status(404).send('The customer with given id is not found')
-    res.send(customer)
+        })
+        customer = await customer.save()
+        res.send(customer)
+    }
+    catch (err) {
+        res.status(404).send('Some error occured')
+    }
 })
 
-router.delete('/:id', async(req,res) => {
-    const customer = await Customer.findByIdAndDelete(req.params.id)
-    if(!customer) return res.status(404).send('Customer with given id not found')
+router.put('/:id', async (req, res) => {
+    const { error } = validate(req.body)
+    if (error) return res.status(400).send(error.details[0].message)
 
-    res.send(customer)
+    try {
+        const customer = await Customer.findByIdAndUpdate(req.params.id,
+            {
+                name: req.body.name,
+                isGold: req.body.isGold,
+                phone: req.body.phone
+            }, { new: true })
+        return res.send(customer)
+    }
+    catch (err) {
+        return res.status(404).send('The customer with given id is not found')
+    }
 })
 
-router.get('/:id', async(req,res) => {
-    const customer = await Customer.findById(req.params.id)
-    if(!customer) return res.status(404).send('The customer with given id was not found')
+router.delete('/:id', async (req, res) => {
+    try {
+        const customer = await Customer.findByIdAndDelete(req.params.id)
+        res.send(customer)
+    }
+    catch (err) {
+        return res.status(404).send('Customer with given id not found')
+    }
+})
 
+router.get('/:id', async (req, res) => {
+    try {
+        const customer = await Customer.findById(req.params.id)
+        res.send(customer)
+    }
+    catch (err) {
+        return res.status(404).send('The customer with given id was not found')
+    }
     res.send(customer)
 })
 
--- routes/genres.js
+++ routes/genres.js
@@ -3,50 +3,59 @@ const mongoose = require('mongoose')
 const express = require('express')
 const router = express.Router()
 
-router.get('/', async(req,res) => {
-    const genres = await Genre.find().sort('name')
-    res.send(genres)
+router.get('/', async (req, res) => {
+    try {
+        const genres = await Genre.find().sort('name')
+        res.send(genres)
+    }
+    catch (err) {
+        res.status(404).send('Some error occured')
+    }
 })
 
-router.post('/', async(req,res) => {
+router.post('/', async (req, res) => {
     const { error } = validate(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    if (error) return res.status(400).send(error.details[0].message)
 
-    let genre = new Genre({ name: req.body.name })
-    genre = await genre.save()
-
-    res.send(genre)
+    try {
+        let genre = new Genre({ name: req.body.name })
+        genre = await genre.save()
+        res.send(genre)
+    }
+    catch (err) {
+        res.status(404).send('Some error occured')
+    }
 })
 
-router.put('/:id', async(req,res) => {
+router.put('/:id', async (req, res) => {
     const { error } = validate(req.body)
-    if(error) return res.status(400).send(error.details[0].message)
+    if (error) return res.status(400).send(error.details[0].message)
 
     try {
         const genre = await Genre.findByIdAndUpdate(req.params.id, { name: req.body.name })
         return res.send(genre)
     }
-    catch(err){
+    catch (err) {
         return res.status(404).send('The genre with the given id was not found')
     }
 })
 
-router.delete('/:id', async(req,res) => {
-    try{
+router.delete('/:id', async (req, res) => {
+    try {
         const genre = await Genre.findByIdAndDelete(req.params.id)
         return res.send(genre)
     }
-    catch(err){
+    catch (err) {
         return res.status(404).send('The genre with the given id was not found')
     }
 })
 
-router.get('/:id', async(req,res) => {
-    try{
+router.get('/:id', async (req, res) => {
+    try {
         const genre = await Genre.findById(req.params.id)
         return res.send(genre)
     }
-    catch(err){
+    catch (err) {
         return escape.status(404).send('The genre with the given id was not found')
     }
 })
